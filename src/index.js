3e909d3d8639bea01e28c08cf12386188a87fd35
dani17r <dannymetal17@gmail.com>
2024-04-23 18:23:13 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import { Client } from '@notionhq/client';
+import cron from 'node-cron';
+
+const notion = new Client({ auth:process.env.API_KEY });
+
+const action = async ()=> {
+  try {
+    console.log("log de ejecucion: "+new Date());
+    const pages = await notion.databases.query({
+        database_id: process.env.DATABASE_ID,
+    });
+  
+    for (const page of pages.results) {
+      await notion.pages.update({
+        page_id: page.id,
+        properties: {
+          estado: {
+            type: 'checkbox',
+            checkbox: false,
+          },
+        },
+      });
+    }
+  }
+  catch(err) {
+    console.log("Hubo un error: \n\t"+err);
+  }
+};
+
+// const options =  {
+//    scheduled: true,
+//    timezone: "America/Sao_Paulo"
+// }
+
+const hourZero = '0 0 * * *';
+cron.schedule(hourZero, action /*, options*/);
