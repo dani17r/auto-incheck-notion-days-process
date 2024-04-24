86d8212a23d5e75bb64faffde44a7828f5a003ff
dani17r <dannymetal17@gmail.com>
2024-04-24 11:04:33 -0400

    test de tiempo

--- src/index.js
+++ src/index.js
@@ -35,8 +35,8 @@ const action = async ()=> {
 //    timezone: "America/Sao_Paulo"
 // }
 
-const hourZero = '0 0 * * *';
-// const hourZero = '* * * * *';
+// const hourZero = '0 0 * * *';
+const hourZero = '* * * * *';
 cron.schedule(hourZero, action /*, options*/);
 
 
